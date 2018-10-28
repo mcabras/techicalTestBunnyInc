ef8a4cf53660554b3bfa28ef9adba00e1fc6827c
Maria Cabra <mcabras@psl.com.co>
2018-10-28 18:20:44 -0500

    Add type

--- bunny-integration-services/src/main/java/com/bunny/integration/service/MergeProfilesServiceImpl.java
+++ bunny-integration-services/src/main/java/com/bunny/integration/service/MergeProfilesServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.bunny.integration.constants.Constants;
 import com.bunny.integration.dto.RequestDto;
 import com.bunny.integration.dto.ResultDto;
 import com.bunny.integration.flow.MergeGateway;
@@ -29,11 +30,13 @@ public class MergeProfilesServiceImpl implements IMergeProfilesService {
 		List<RequestDto> lstRequest = new ArrayList<>();
 		RequestDto objRequestBio = RequestDto.builder()
 				.personId(profileId)
+				.type(Constants.BIO)
 				.url(urlBio).build();
 		lstRequest.add(objRequestBio);
 		
 		RequestDto objRequestLinked = RequestDto.builder()
 				.accessToken(token)
+				.type(Constants.LINKEDIN)
 				.url(urlLinked).build();
 		lstRequest.add(objRequestLinked);
 		
